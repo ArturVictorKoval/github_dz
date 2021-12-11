a40953e95169b42f6df761f2004389a83d3621d8
artur <kovalartur@gmail.com>
2021-12-11 16:52:10 +0200

    dobavil domashka6.4

---
+++ dz/domashka6.4.py
@@ -0,0 +1,22 @@
+# Калькулятор
+import math
+what = input("Выберите операцию (+,-,/,*): ")
+a = float(input("введите первое число: "))
+b = float(input("введите второе число: "))
+if what == "+":
+    c = a + b
+    print("Результат: " + str(c))
+elif what == "-":
+    c = a - b
+    print("Результат: " + str(c))
+elif what == "/":
+    if b ==0:
+        print("на ноль делить нельзя")
+    else:
+        c = a / b
+        print("Результат: " + str(c))
+elif what== "*":
+    c = a * b
+    print("Результат: " + str(c))
+else:
+    print("Выбрана неверная операция!")
